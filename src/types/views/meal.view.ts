109fcb0e25729c136111e83df5f0beb70659a9f6
edgarciavfx <edgarciavfx@gmail.com>
2026-06-10 19:36:04 -0600

    feat: add React Query hooks for meals and ingredients

--- src/types/views/meal.view.ts
+++ src/types/views/meal.view.ts
@@ -1,9 +1,32 @@
+export interface IngredientView {
+  id: string;
+  mealId: string;
+  name: string;
+  quantity: number;
+  unit: string;
+}
+
+export interface MealDetailView {
+  id: string;
+  name: string;
+  preparationMinutes: number;
+  archived: boolean;
+  ingredients: IngredientView[];
+}
+
 export interface MealSummaryView {
   id: string;
   name: string;
   preparationMinutes: number;
   ingredientCount: number;
   archived: boolean;
+}
+
+export interface MealLibraryResult {
+  meals: MealDetailView[];
+  total: number;
+  page: number;
+  pageSize: number;
 }
 
 export interface MealRecommendationView {
